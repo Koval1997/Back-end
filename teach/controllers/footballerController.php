0d3cbca39c40bbe9701ac8953ce4af62a8913638
Koval1997 <kovalmax06@gmail.com>
2017-11-08 02:15:09 +0400

    Update footballerController.php

--- teach/controllers/footballerController.php
+++ teach/controllers/footballerController.php
@@ -32,13 +32,12 @@ class footballerController extends Controller {
 
     public function edit($data) {
 
-        $_PUT=json_decode(file_get_contents('php://input'));
-
-        if( (isset($_POST['id'])) && (isset($_POST['name'])) && (isset($_POST['image']))
-            && (isset($_POST['power'])) && (isset($_POST['speed'])) ) {
+        $_PUT=json_decode(file_get_contents('php://input'),true);
+        if( (isset($_PUT['id'])) && (isset($_PUT['name'])) && (isset($_PUT['image']))
+            && (isset($_PUT['power'])) && (isset($_PUT['speed'])) ) {
             // мы передаем в модель массив с данными
             // модель должна вернуть boolean
-            $dataToUpd=array('id'=>$_POST['id'], 'name'=>$_POST['name'], 'image'=>$_POST['name'], 'power'=>$_POST['name'], 'speed'=>$_POST['score']);
+            $dataToUpd=array('id'=>$_PUT['id'], 'name'=>$_PUT['name'], 'image'=>$_PUT['name'], 'power'=>$_PUT['name'], 'speed'=>$_PUT['score']);
             $updItem=$this->model->save($dataToUpd, $data['id']);
             $this->setResponce($updItem);
         }
